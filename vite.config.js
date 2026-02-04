6f7c9370bf18fae7727c3771237dc1ad7a0240bf
Amit Chaudhary <chaudharyamitiit2007@gmail.com>
2026-02-04 18:40:16 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about-us/index.html"),
+        accounting: resolve(
+          __dirname,
+          "accounting-finance-consulting-cfo-services-ras-al-khaimah-uae/index.html"
+        ),
+        taxSupport: resolve(
+          __dirname,
+          "tax-support-corporate-tax-vat-consultancy-ras-al-khaimah-uae/index.html"
+        ),
+        financialPlanning: resolve(
+          __dirname,
+          "financial-planning-cost-control-business-setup-ras-al-khaimah-uae/index.html"
+        ),
+        compliance: resolve(
+          __dirname,
+          "compliance-aml-due-diligence-esr-risk-management-ras-al-khaimah-uae/index.html"
+        ),
+        contact: resolve(__dirname, "contact-us/index.html")
+      }
+    }
+  }
+});
