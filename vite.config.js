940cc3f474f65b1961dc6be0150bb2c69e67d36f
Amit Chaudhary <chaudharyamitiit2007@gmail.com>
2026-02-04 20:18:09 +0530

    updated references

--- vite.config.js
+++ vite.config.js
@@ -6,25 +6,29 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about-us.html"),
+        main: resolve(__dirname, "index", "index.html"),
+        about: resolve(__dirname, "about-us", "index.html"),
         accounting: resolve(
           __dirname,
-          "accounting-finance-consulting-cfo-services-ras-al-khaimah-uae.html"
+          "accounting-finance-consulting-cfo-services-ras-al-khaimah-uae",
+          "index.html"
         ),
         taxSupport: resolve(
           __dirname,
-          "tax-support-corporate-tax-vat-consultancy-ras-al-khaimah-uae.html"
+          "tax-support-corporate-tax-vat-consultancy-ras-al-khaimah-uae",
+          "index.html"
         ),
         financialPlanning: resolve(
           __dirname,
-          "financial-planning-cost-control-business-setup-ras-al-khaimah-uae.html"
+          "financial-planning-cost-control-business-setup-ras-al-khaimah-uae",
+          "index.html"
         ),
         compliance: resolve(
           __dirname,
-          "compliance-aml-due-diligence-esr-risk-management-ras-al-khaimah-uae.html"
+          "compliance-aml-due-diligence-esr-risk-management-ras-al-khaimah-uae",
+          "index.html"
         ),
-        contact: resolve(__dirname, "contact-us.html")
+        contact: resolve(__dirname, "contact-us", "index.html")
       }
     }
   }
