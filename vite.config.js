4c6a62071e0a22dfcfe9f553474f0752908e1f5a
Amit Chaudhary <chaudharyamitiit2007@gmail.com>
2026-02-04 19:33:45 +0530

    added github actions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
@@ -7,24 +7,24 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about-us/index.html"),
+        about: resolve(__dirname, "about-us.html"),
         accounting: resolve(
           __dirname,
-          "accounting-finance-consulting-cfo-services-ras-al-khaimah-uae/index.html"
+          "accounting-finance-consulting-cfo-services-ras-al-khaimah-uae.html"
         ),
         taxSupport: resolve(
           __dirname,
-          "tax-support-corporate-tax-vat-consultancy-ras-al-khaimah-uae/index.html"
+          "tax-support-corporate-tax-vat-consultancy-ras-al-khaimah-uae.html"
         ),
         financialPlanning: resolve(
           __dirname,
-          "financial-planning-cost-control-business-setup-ras-al-khaimah-uae/index.html"
+          "financial-planning-cost-control-business-setup-ras-al-khaimah-uae.html"
         ),
         compliance: resolve(
           __dirname,
-          "compliance-aml-due-diligence-esr-risk-management-ras-al-khaimah-uae/index.html"
+          "compliance-aml-due-diligence-esr-risk-management-ras-al-khaimah-uae.html"
         ),
-        contact: resolve(__dirname, "contact-us/index.html")
+        contact: resolve(__dirname, "contact-us.html")
       }
     }
   }
